c47d646f7a50f635c61a07e7fe77a7b4ea0a444b
hqc <1848023572@qq.com>
2020-09-14 23:28:51 +0800

    添加了一个组件

---
+++ src/views/movie/movie.vue
@@ -0,0 +1,25 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    components:{
+
+    },
+    created:{
+
+    },
+    mounted:{
+        
+    }
+};
+</script>
+
+<style>
+</style>
